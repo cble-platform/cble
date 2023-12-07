97f58d047c310cde78db0ce052bfa78c5f66a195
BradHacker <harkerbd@gmail.com>
2023-12-07 15:33:16 -0500

    Add GQL codegen and basic UI

---
+++ src/colors.ts
@@ -0,0 +1,10 @@
+import { PaletteOptions } from "@mui/material";
+
+export const CBLEPalette: PaletteOptions = {
+  primary: {
+    main: "#F76902",
+  },
+  secondary: {
+    main: "#D0D3D4",
+  },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// Styles
+import "./index.css";
+// Fonts
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Pages
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Login from "./routes/auth/login";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeWrapper } from "./theme";
+import { client } from "./api/apollo";
+import { ApolloProvider } from "@apollo/client";
+import EditorTest from "./routes/editor-test";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "editor-test",
+        element: <EditorTest />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/auth",
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <ThemeWrapper>
+        <RouterProvider router={router} />
+      </ThemeWrapper>
+    </ApolloProvider>
+  </React.StrictMode>
+);
